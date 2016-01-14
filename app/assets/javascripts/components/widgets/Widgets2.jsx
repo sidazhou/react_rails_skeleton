8eaf861ccfc52cd1bf504f19070920ba01541799
sidazhou <sidazhou@gmail.com>
2016-01-14 10:19:18 -0800

    added react-bootstrap

--- app/assets/javascripts/components/widgets/Widgets2.jsx
+++ app/assets/javascripts/components/widgets/Widgets2.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 var Widgets2 = React.createClass({
-  getInitialState: function() {
-    return {liked: false};
-  },
-  handleClick: function(event) {
-    this.setState({liked: !this.state.liked});
-  },
   render: function() {
-    var text = this.state.liked ? 'like' : 'haven\'t liked';
     return (
-      <p onClick={this.handleClick}>
-        You {text} this. Click to toggle. -Widgets2
-      </p>
+      <Button bsStyle="primary">Primary</Button>
     );
   }
 });
 
 export default Widgets2;
+
+// import React from 'react';
+
+// var Widgets2 = React.createClass({
+//   getInitialState: function() {
+//     return {liked: false};
+//   },
+//   handleClick: function(event) {
+//     this.setState({liked: !this.state.liked});
+//   },
+//   render: function() {
+//     var text = this.state.liked ? 'like' : 'haven\'t liked';
+//     return (
+//       <p onClick={this.handleClick}>
+//         You {text} this. Click to toggle. -Widgets2
+//       </p>
+//     );
+//   }
+// });
+
+// export default Widgets2;
