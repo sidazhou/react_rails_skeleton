a26185bc728d0d6c82df9f09063d98dc81d56135
sidazhou <sidazhou@gmail.com>
2016-01-22 10:35:13 -0800

    minor improvement

--- app/assets/javascripts/components/widgets/Widgets6button.jsx
+++ app/assets/javascripts/components/widgets/Widgets6button.jsx
@@ -9,6 +9,7 @@ function mapStateToProps(state) { // react-redux
 function mapDispatchToProps(dispatch) {
   return {
     handleClick: (filterName, event) => { // omfg? .bind(this, arg1, arg2) maps onto (arg1, arg2, event) https://github.com/facebook/react/issues/3258
+      console.log(this);
       event.preventDefault();
       dispatch({
         type: 'SET_VISIBILITY_FILTER',
@@ -22,8 +23,8 @@ class FilterLink extends React.Component {
 
   render() {
     return (
-      <a href="/should-not-be-reached"
-        onClick = {this.props.handleClick.bind(this, this.props.filter)}
+      <a href="#"
+        onClick = { (event) => {this.props.handleClick(this.props.filter, event)} }
       >
         {this.props.filter}
       </a>
